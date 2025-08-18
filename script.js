cb9c5ebe9d502e3a2d72694590de2f85fa8354be
samsam55 <49875528+samsam55@users.noreply.github.com>
2025-08-18 11:45:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -56,12 +56,14 @@ document.getElementById("B_side_Tx_Fault").onclick = function () {
 
 document.getElementById("A_side_LV_EPB").onclick = function () {
     isFault_A1 = 1
+    isFault_A2 = 1
     isFault_C = 1
     draw_circuit()
 }
 
 document.getElementById("B_side_LV_EPB").onclick = function () {
     isFault_B1 = 1
+    isFault_B2 = 1
     isFault_D = 1
     draw_circuit()
 }
