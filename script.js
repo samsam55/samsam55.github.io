7a3c623f949a2978d006901653fef8bb042ea389
NG Sam Man Sing (吳旻聲) <NGSamMaS@mtr.com.hk>
2025-11-17 13:18:42 +0800

    updated ref

--- script.js
+++ script.js
@@ -46,13 +46,25 @@ document.getElementById("B_side_LV_Fault").onclick = function () {
     draw_circuit()
 }
 
-document.getElementById("A_side_REF_HVEPB").onclick = function () {
+document.getElementById("A_side_HVEPB").onclick = function () {
     isFault_A1 = 1
     draw_circuit()
 }
 
-document.getElementById("B_side_REF_HVEPB").onclick = function () {
+document.getElementById("B_side_HVEPB").onclick = function () {
     isFault_B1 = 1
+    draw_circuit()
+}
+
+document.getElementById("A_side_REF").onclick = function () {
+    isFault_A1 = 1
+    isFault_A2 = 1
+    draw_circuit()
+}
+
+document.getElementById("B_side_REF").onclick = function () {
+    isFault_B1 = 1
+    isFault_B2 = 1
     draw_circuit()
 }
 
